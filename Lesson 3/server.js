d6eea3180186cd26bd1bbcd8eed24b982e402425
200202iqbal <75594145+200202iqbal@users.noreply.github.com>
2021-11-29 11:39:50 +0900

    lesson3:web-server withexpress

---
+++ Lesson 3/server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+
+dotenv.config();
+
+const host = process.env.HOST; 
+const port = process.env.PORT;
+
+const app = express();
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(__dirname+"/public"));
+
+app.post("/auth",(request,response)=>{
+    const login_name = request.body.login_name;
+    const password = request.body.password;
+    let message = ""
+    if(login_name == process.env.LOGINNAME && password == process.env.PASSWORD)
+    {
+        message = ">ログインしました"
+    }
+    else{
+        message = "ログインできません。";
+    } 
+    
+    response.send(message);
+});
+
+//app.get(URL,処理)
+app.get("/",(request,response) => {
+    console.log(request.body);
+    console.log(request.url);
+    console.log(request.query);
+    response.send("Hello Express!");
+});
+
+app.get("/profile",(request,response)=>{
+    response.send("<h1 style:background>This is Profile Page!!!</h1>");
+});
+
+app.listen(port,host, () =>{
+
+    console.log(`Server listen: http://${host}:${port}/login.html`);
+    //他の書き方
+    // console.log("http://"+host+":"+port);
+});
